f7216c4ea0b35d938aabf2f0aff45e289953291f
unknown <omergd@gmail.com>
2020-07-15 22:16:33 +0300

    refactoring

--- src/components/SingleCountry.js
+++ src/components/SingleCountry.js
@@ -1,48 +1,53 @@
-import React, {useState, useEffect} from 'react'
-import {LineChart, Line, Tooltip, Legend,
-  XAxis, YAxis, CartesianGrid} from 'recharts';
-import {getCovidData, getCountryData, getChartMargin} from '../utils/functions'
+import React, { useEffect, useState } from 'react'
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { getChartMargin, getCountryData, getCovidData } from '../utils/functions'
 import DataVisualization from './includes/DataVisualization';
 import tableColumnsOptions from '../utils/tableColumnsOptions'
 
 const SingleCountry = (props) => {
   const [data, setData] = useState([])
   const [countryData, setCountryData] = useState([])
-  const [chartMax, setChartMax] = useState([])
+  const [chartMax, setChartMax] = useState(0)
 
-  useEffect(() => {
-        if(data.length===0){
-          getCovidData(setData)
-        }
-        if(countryData.length===0){
-          getCountryData(data, props.match.params.id, setCountryData)
-        }
-        const lastDateRecord = countryData.find(el=>el.date==='2020-07-01')
-        if(lastDateRecord && !chartMax){
-          setChartMax(lastDateRecord.confirmed_cases)
-        }
+    useEffect(()=>{
+        getCovidData().then((data) => {
+            setData(data);
+        });
+    }, []);
 
-  },[data, countryData, props.match.params.id, chartMax])
+    useEffect(()=>{
+        if (!countryData.length && data.length) {
+            const newCountryData = getCountryData(data, props.match.params.id);
+            setCountryData(newCountryData);
+        }
+    }, [data, props.match.params.id])
+
+    useEffect(()=>{
+        if (!chartMax.length) {
+            const lastDateRecord = countryData.find(el => el.date === "2020-07-01")
+            if (lastDateRecord) {
+                setChartMax(lastDateRecord.confirmed_cases)
+            }
+        }
+    }, [countryData, chartMax.length]);
 
   const defaultSorted = [{
     dataField: 'date',
     order: 'asc'
   }]
 
-  console.log(chartMax)
-
   return(
-    <DataVisualization 
-      topTenCases={countryData} 
-      defaultSorted={defaultSorted} 
-      columns={tableColumnsOptions('singleCountry')} 
+    <DataVisualization
+      topTenCases={countryData}
+      defaultSorted={defaultSorted}
+      columns={tableColumnsOptions('singleCountry')}
       title={`Confirmed cases in ${props.match.params.countryName}`}
       keyField={'date'}
     >
       <LineChart data={countryData} margin={getChartMargin()}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="day"/>
-        <YAxis tickFormatter={tick => tick.toLocaleString()} domain={[0,'dataMax+20']}/>
+        <YAxis tickFormatter={tick => tick.toLocaleString()} domain={[0, `dataMax + ${chartMax}`]} />
         <Tooltip formatter={(value) => new Intl.NumberFormat('en').format(value)}/>
         <Legend />
         <Line type="monotone" dataKey="confirmed_cases" fill="#2d7bbd" activeDot={{ r: 8 }}  name="Confirmed cases"/>
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,64 +1,72 @@
 export const getCovidData = async (setData) => {
-    const proxy = 'https://cors-anywhere.herokuapp.com/'
-    const endPoint = 'https://static-cdn.workiz.com/various/covid-may-jul.json'
-    const res = await fetch(proxy + endPoint)//fetch request via proxy, to bypass CORS incompatibility on the client without server setup
-    const data = await res.json()
-    setData(data)
+  const proxy = 'https://cors-anywhere.herokuapp.com/'
+  const endPoint = 'https://static-cdn.workiz.com/various/covid-may-jul.json'
+  const res = await fetch(proxy + endPoint)//fetch request via proxy, to bypass CORS incompatibility on the client without server setup
+  const data = await res.json()
+  if (typeof setData === "function") {
+      setData(data)
+  } else {
+      return data;
   }
-  
+}
+
 export const getTopTenCases = (data) => {
-    return data
-        .filter(el=>el.date==="2020-07-01")
-        .sort((a,b)=>b.confirmed_cases-a.confirmed_cases)
-        .slice(0,10)
-        .map(el=>{
-        return {
-            ...el,
-            country:el.countries_and_territories.split('_').join(' '),
-            confirmed_cases:Number(el.confirmed_cases), //used for chart, thousands separator added later
-            confirmed_cases_formatted:Number(el.confirmed_cases).toLocaleString('en') //used for table, thousands separator added here
-        }
-        })
-    
-  }
-
-  export const getIncreaseRates = (data, isStable) => {
-    let starterRecord
-    return data.filter(el=>el.date==="2020-05-01" || el.date==="2020-07-01").reduce((acc,curr,idx,arr)=>{
-        if(curr.date==="2020-07-01"){
-            if(arr[idx-1].date!=="2020-05-01"){//there are two countries, Lesotho and Comoros), that their earliest record is not "2020-05-01"
-                starterRecord=data.find(item=>item.countries_and_territories===curr.countries_and_territories)//find earliest record for the above countries
-            }else{
-                starterRecord=arr[idx-1]
-            }
-          acc.push(
-            {
-              country:curr.countries_and_territories.split('_').join(' '),
-              geo_id:curr.geo_id,
-              increaseRate:(Math.round((curr.confirmed_cases/starterRecord.confirmed_cases) * 100) / 100).toFixed(2),//round to 2 decimals
-              max:Number(curr.confirmed_cases).toLocaleString('en'),
-              min:starterRecord.confirmed_cases
-            }
-          )
-        }
-        return acc
-      },[]).sort((a,b)=>isStable?a.increaseRate-b.increaseRate:b.increaseRate-a.increaseRate).slice(0,10)
-      
-  }
-
-  export const getCountryData = (data, id, setCountryData) => {
-    const countryData = data
-      .filter(el=>el.geo_id===id)
-      .map(el=>{
-        return{
+  return data
+    .filter(el=>el.date==="2020-07-01")
+    .sort((a,b)=>b.confirmed_cases-a.confirmed_cases)
+    .slice(0,10)
+    .map(el=>{
+      return {
           ...el,
-          confirmed_cases_formatted:Number(el.confirmed_cases).toLocaleString('en'),
-          country:el.countries_and_territories.split('_').join(' ')
-        }
-      })
-      setCountryData(countryData)
-  }
+          country:el.countries_and_territories.split('_').join(' '),
+          confirmed_cases:Number(el.confirmed_cases), //used for chart, thousands separator added later
+          confirmed_cases_formatted:Number(el.confirmed_cases).toLocaleString('en') //used for table, thousands separator added here
+      }
+    })
+}
 
-  export const getChartMargin = () => {
-    return {top: 10, right: 30, left: 15, bottom: 0}
-  }
+export const getIncreaseRates = (data, isStable) => {
+  let starterRecord
+  return data.filter(el=>el.date==="2020-05-01" || el.date==="2020-07-01").reduce((acc,curr,idx,arr)=>{
+      if(curr.date==="2020-07-01"){
+          if(arr[idx-1].date!=="2020-05-01"){//there are two countries, Lesotho and Comoros, that their earliest record is not "2020-05-01"
+              starterRecord=data.find(item=>item.countries_and_territories===curr.countries_and_territories)//find earliest record for the above countries
+          }else{
+              starterRecord=arr[idx-1]
+          }
+        acc.push(
+          {
+            country:curr.countries_and_territories.split('_').join(' '),
+            geo_id:curr.geo_id,
+            increaseRate:(Math.round((curr.confirmed_cases/starterRecord.confirmed_cases) * 100) / 100).toFixed(2),//round to 2 decimals
+            max:Number(curr.confirmed_cases).toLocaleString('en'),
+            min:starterRecord.confirmed_cases
+          }
+        )
+      }
+      return acc
+    },[]).sort((a,b)=>isStable?a.increaseRate-b.increaseRate:b.increaseRate-a.increaseRate).slice(0,10)
+
+}
+
+export const getCountryData = (data, id, setCountryData) => {
+  const countryData = data
+    .filter(el=>el.geo_id===id)
+    .map(el=>{
+      return{
+        ...el,
+        confirmed_cases_formatted:Number(el.confirmed_cases).toLocaleString('en'),
+        country:el.countries_and_territories.split('_').join(' ')
+      }
+    })
+
+    if (typeof setCountryData === "function") {
+        setCountryData(countryData);
+    } else {
+        return countryData;
+    }
+}
+
+export const getChartMargin = () => {
+  return {top: 10, right: 30, left: 15, bottom: 0}
+}
